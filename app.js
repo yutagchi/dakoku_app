9a0d8b8d761c5c1b4d12aab759bb871d389645b9
tagchl <103634537+tagchl@users.noreply.github.com>
2022-04-14 22:30:50 +0900

    時間だけは出せるようになった（リアルタイム更新ができない）

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
+  let nowTime = new Date();
+  let nowHour = nowTime.getHours();
+  let nowMin  = nowTime.getMinutes();
+  let nowSec  = nowTime.getSeconds();
+  let now = nowHour + ":" + nowMin + ":" + nowSec;
+
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+  res.render('hello.ejs',{timeStamp: now});
 });
 
 app.listen(3000);
