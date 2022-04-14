afa7a0ad22feb40b9899ecc7fc4111c6ce9b996c
tagchl <103634537+tagchl@users.noreply.github.com>
2022-04-15 00:19:41 +0900

    リロードで時刻も更新されるように

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
   let nowTime = new Date();
   let nowHour = nowTime.getHours();
   let nowMin  = nowTime.getMinutes();
   let nowSec  = nowTime.getSeconds();
   let now = nowHour + ":" + nowMin + ":" + nowSec;
-
-app.get('/', (req, res) => {
-  res.render('hello.ejs',{timeStamp: now});
+  res.render('hello.ejs',{now: now});
 });
 
 app.listen(3000);
