6e03b2187c183e549b733d0b85e3fd510508336a
tagchl <103634537+tagchl@users.noreply.github.com>
2022-04-15 12:56:43 +0900

    mysql起動成功！！

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'yuki',
+  database: 'list_app'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
 
 app.get('/', (req, res) => {
   let nowTime = new Date();
@@ -9,6 +25,16 @@ app.get('/', (req, res) => {
   let nowSec  = nowTime.getSeconds();
   let now = nowHour + ":" + nowMin + ":" + nowSec;
   res.render('hello.ejs',{now: now});
+});
+
+app.get('/sql', (req, res) => {
+  connection.query(
+    'SELECT * FROM users',
+    (error, results) => {
+      console.log(results);
+      res.render('sqlhello.ejs');
+    }
+  );
 });
 
 app.listen(3000);
