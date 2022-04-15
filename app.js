edc2ddedca6293c000b6bd9bec38cd2f998a9e10
tagchl <103634537+tagchl@users.noreply.github.com>
2022-04-16 00:36:26 +0900

    mysqlに打刻できるようになった

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'yuki',
-  database: 'list_app'
+  database: 'timecard_app'
 });
 
 connection.connect((err) => {
@@ -19,12 +19,45 @@ connection.connect((err) => {
 });
 
 app.get('/', (req, res) => {
-  let nowTime = new Date();
-  let nowHour = nowTime.getHours();
-  let nowMin  = nowTime.getMinutes();
-  let nowSec  = nowTime.getSeconds();
-  let now = nowHour + ":" + nowMin + ":" + nowSec;
-  res.render('hello.ejs',{now: now});
+  let getTime = new Date();
+  let nowHour = getTime.getHours();
+  let nowMin  = getTime.getMinutes();
+  let nowSec  = getTime.getSeconds();
+  let nowTime = nowHour + ":" + nowMin + ":" + nowSec;
+  res.render('hello.ejs',{now: nowTime});
+});
+
+app.get('/begin', (rew, res) => {
+  let getTime = new Date();
+  connection.query(
+    'INSERT INTO timestamps (username,status,timestamp) VAlUES("yuki","bigin",?)',
+    [getTime],
+    (error, results) => {
+      console.log(nowTime);
+      res.redirect('/');
+    }
+  );
+});
+
+app.get('/finish', (rew, res) => {
+  let getTime = new Date();
+  connection.query(
+    'INSERT INTO timestamps (username,status,timestamp) VAlUES("yuki","finish",?)',
+    [getTime],
+      (error, results) => {
+      res.redirect('/');
+    }
+  );
+});
+
+app.get('/timestamp_list', (rew, res) => {
+connection.query(
+  'SELECT * FROM timestamps',
+  (error, results)=> {
+  res.render('timestamp_list.ejs',{timestamps: results[0]});
+  }
+);
+  
 });
 
 app.get('/sql', (req, res) => {
